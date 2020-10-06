316c10ae9d39191c3c2da3d3e9d48199d05226a0
YusukeKishino <kishi.aqua.77@gmail.com>
2020-10-07 05:42:54 +0900

    Define post status

--- model/post.go
+++ model/post.go
@@ -2,11 +2,19 @@ package model
 
 import "time"
 
+type PostStatus string
+
+const (
+	Draft       PostStatus = "draft"
+	Published   PostStatus = "published"
+	Unpublished PostStatus = "unpublished"
+)
+
 type Post struct {
-	ID        uint   `gorm:"primary_key"`
-	Title     string `gorm:"size:255;not null"`
-	Content   string `gorm:"type:text not null"`
-	Status    string `gorm:"size:255;not null"`
+	ID        uint       `gorm:"primary_key"`
+	Title     string     `gorm:"size:255;not null"`
+	Content   string     `gorm:"type:text not null"`
+	Status    PostStatus `gorm:"size:255;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
