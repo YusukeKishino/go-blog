322cdc057e659730226e35c128430f5a4d94bcd7
YusukeKishino <kishi.aqua.77@gmail.com>
2020-10-10 05:23:50 +0900

    Fix post body

--- server/func.go
+++ server/func.go
@@ -2,13 +2,24 @@ package server
 
 import (
 	"html/template"
+	"regexp"
 
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
 )
 
+var regxNewline = regexp.MustCompile(`\r\n|\r|\n`)
+
 func MD2HTML(md string) template.HTML {
-	return template.HTML(markdown.ToHTML([]byte(md), nil, renderer()))
+	replaced := regxNewline.ReplaceAllString(md, "\n")
+	return template.HTML(markdown.ToHTML([]byte(replaced), getParser(), renderer()))
+}
+
+func getParser() *parser.Parser {
+	return parser.NewWithExtensions(
+		parser.CommonExtensions | parser.HardLineBreak,
+	)
 }
 
 func renderer() markdown.Renderer {
