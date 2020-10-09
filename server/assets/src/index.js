667cae8513c764c6a020d1c42a6701b867387a9e
YusukeKishino <kishi.aqua.77@gmail.com>
2020-10-10 07:01:46 +0900

    Highlight code

--- server/assets/src/index.js
+++ server/assets/src/index.js
@@ -1,2 +1,6 @@
-import 'style/style.css'
+import 'style/style.scss'
 import '@fortawesome/fontawesome-free/js/all'
+
+import 'highlight.js/styles/github.css'
+import hljs from 'highlight.js'
+hljs.initHighlightingOnLoad()
