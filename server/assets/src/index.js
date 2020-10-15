6f8946a704fbb745b3129c5c7e5a801ba1a83718
YusukeKishino <kishi.aqua.77@gmail.com>
2020-10-16 02:40:07 +0900

    Refactor highlight js

--- server/assets/src/index.js
+++ server/assets/src/index.js
@@ -1,8 +1,5 @@
 import 'style/style.scss'
 import '@fortawesome/fontawesome-free/js/all'
 
-import 'highlight.js/styles/github.css'
-import hljs from 'highlight.js'
-hljs.initHighlightingOnLoad()
-
+import 'js/codehighlight'
 import 'js/upload'
